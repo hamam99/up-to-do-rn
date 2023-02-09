890a37eb3038d1ecdc8993a62769a9510c33b329
Hamam <zero.saturnus@gmail.com>
2023-02-09 14:03:22 +0700

    add color white variant

--- src/utils/Colors.tsx
+++ src/utils/Colors.tsx
@@ -1,8 +1,10 @@
 const Colors = {
   purple: '#8875FF',
   purple2: '#8687E7',
+  white: '#ffffff',
   white1: 'rgba(255, 255, 255, 0.87)',
   white2: 'rgba(255, 255, 255, 0.44)',
+  white3: 'rgba(255, 255, 255, 0.67)',
   gray1: '#AFAFAF',
   gray2: '#979797',
   black: '#121212',
