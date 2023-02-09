38ee8ace9742778987d4425b182c1d385ee09887
Hamam <zero.saturnus@gmail.com>
2023-02-09 13:39:59 +0700

    refactoring

--- src/pages/StartOnboard/index.tsx
+++ src/pages/StartOnboard/index.tsx
@@ -1,122 +1,13 @@
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react';
 import {Colors, Fonts} from '../../utils';
 import Header from '../../components/atoms/Header';
 import {useNavigation} from '@react-navigation/native';
-import {Input} from '@rneui/themed';
 import GlobalStyles from '../../styles';
-import {ButtonCustom, TextInput, TextInputCustom} from '../../components';
+import {ButtonCustom, TextInputCustom} from '../../components';
 
 const StartOnboard = () => {
   const navigation = useNavigation();
-
-  const FormLogin = () => {
-    return (
-      <>
-        <View style={{marginTop: 40}}>
-          <TextInputCustom
-            placeholder="Username"
-            label={'Username'}
-            onChangeText={text => {}}
-          />
-          <TextInputCustom
-            placeholder="Password"
-            label={'Password'}
-            onChangeText={text => {}}
-          />
-        </View>
-        <ButtonCustom
-          title={'Login'}
-          onPress={() => {}}
-          containerStyle={{marginTop: 24}}
-        />
-      </>
-    );
-  };
-  const Divider = () => {
-    return (
-      <View
-        style={{
-          marginVertical: 32,
-          flexDirection: 'row',
-          height: 24,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <View style={{backgroundColor: Colors.gray2, height: 1, flexGrow: 1}} />
-        <Text
-          style={{
-            color: Colors.gray2,
-            marginHorizontal: 4,
-            fontSize: 16,
-            fontFamily: Fonts.LatoRegular,
-          }}>
-          or
-        </Text>
-        <View style={{backgroundColor: Colors.gray2, height: 1, flexGrow: 1}} />
-      </View>
-    );
-  };
-
-  const SocialButton = () => {
-    return (
-      <>
-        <ButtonCustom
-          title={'Login With Google'}
-          type={'outline'}
-          icon={{
-            name: 'google',
-            type: 'font-awesome',
-            size: 15,
-            color: 'white',
-          }}
-          onPress={() => {}}
-        />
-
-        <ButtonCustom
-          title={'Login With Apple'}
-          type={'outline'}
-          icon={{
-            name: 'apple',
-            type: 'font-awesome',
-            size: 15,
-            color: 'white',
-          }}
-          containerStyle={{
-            marginTop: 20,
-          }}
-          onPress={() => {}}
-        />
-      </>
-    );
-  };
-
-  const RegisterButton = () => {
-    return (
-      <View
-        style={{flexDirection: 'row', justifyContent: 'center', marginTop: 16}}>
-        <Text
-          style={{
-            fontSize: 12,
-            fontFamily: Fonts.LatoRegular,
-            color: Colors.gray2,
-          }}>
-          Don’t have an account?
-        </Text>
-        <TouchableOpacity>
-          <Text
-            style={{
-              fontSize: 12,
-              fontFamily: Fonts.LatoRegular,
-              color: 'white',
-              marginLeft: 4,
-            }}>
-            Register
-          </Text>
-        </TouchableOpacity>
-      </View>
-    );
-  };
 
   return (
     <View style={GlobalStyles.container}>
@@ -135,13 +26,110 @@ const StartOnboard = () => {
         }}>
         Login
       </Text>
+      <View style={styles.formContainer}>
+        <TextInputCustom
+          placeholder="Username"
+          label={'Username'}
+          onChangeText={() => {}}
+        />
+        <TextInputCustom
+          placeholder="Password"
+          label={'Password'}
+          onChangeText={text => {}}
+        />
+      </View>
+      <ButtonCustom
+        title={'Login'}
+        onPress={() => {}}
+        containerStyle={styles.formButton}
+      />
 
-      <FormLogin />
-      <Divider />
-      <SocialButton />
-      <RegisterButton />
+      <View style={styles.dividerWrapper}>
+        <View style={styles.dividerLine} />
+        <Text style={styles.dividerText}>or</Text>
+        <View style={styles.dividerLine} />
+      </View>
+
+      {/* SOCIAL BUTTON */}
+      <ButtonCustom
+        title={'Login With Google'}
+        type={'outline'}
+        icon={{
+          name: 'google',
+          type: 'font-awesome',
+          size: 15,
+          color: 'white',
+        }}
+        onPress={() => {}}
+      />
+
+      <ButtonCustom
+        title={'Login With Apple'}
+        type={'outline'}
+        icon={{
+          name: 'apple',
+          type: 'font-awesome',
+          size: 15,
+          color: 'white',
+        }}
+        containerStyle={{
+          marginTop: 20,
+        }}
+        onPress={() => {}}
+      />
+
+      {/* REGISTER BUTTON */}
+      <View style={styles.registerWrapper}>
+        <Text style={styles.registerAccount}>Don’t have an account?</Text>
+        <TouchableOpacity>
+          <Text style={styles.registerButton}>Register</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 export default StartOnboard;
+
+const styles = StyleSheet.create({
+  formContainer: {
+    marginTop: 40,
+  },
+  formButton: {
+    marginTop: 24,
+  },
+  dividerWrapper: {
+    marginVertical: 32,
+    flexDirection: 'row',
+    height: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dividerLine: {
+    backgroundColor: Colors.gray2,
+    height: 1,
+    flexGrow: 1,
+  },
+  dividerText: {
+    color: Colors.gray2,
+    marginHorizontal: 4,
+    fontSize: 16,
+    fontFamily: Fonts.LatoRegular,
+  },
+  registerWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 16,
+  },
+  registerAccount: {
+    fontSize: 12,
+    fontFamily: Fonts.LatoRegular,
+    color: Colors.gray2,
+  },
+  registerButton: {
+    fontSize: 12,
+    fontFamily: Fonts.LatoRegular,
+    color: 'white',
+    marginLeft: 4,
+  },
+});
