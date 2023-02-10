bdd88ce4f203332d51e8d0db3a7341f9770cf106
Hamam <zero.saturnus@gmail.com>
2023-02-10 15:10:03 +0700

    add validation and auth

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,4 +1,6 @@
+import Auth from './Auth';
 import Colors from './Colors';
 import Fonts from './Fonts';
+import Validation from './Validation';
 
-export {Colors, Fonts};
+export {Colors, Fonts, Validation, Auth};
