81f3b98d3c0a43a1f1deb3e4c982fc094e5ddf9c
Hamam <zero.saturnus@gmail.com>
2023-02-14 12:26:03 +0700

    refactoring

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import {View, Text, FlatList, Image, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  ImageProps,
+  ImageSourcePropType,
+} from 'react-native';
 import {HeaderWithProfile} from '../../components';
 import {CategoryData, HomeData} from '../../data';
 import GlobalStyles from '../../styles';
@@ -40,25 +48,8 @@ const Items = ({
   });
 
   return (
-    <View
-      style={{
-        backgroundColor: Colors.black2,
-        minHeight: 72,
-        borderRadius: 4,
-        alignItems: 'center',
-        padding: 8,
-        flexDirection: 'row',
-      }}>
-      <View
-        style={{
-          borderRadius: 8,
-          width: 16,
-          height: 16,
-          borderWidth: 1,
-          borderColor: 'white',
-          marginRight: 12,
-        }}
-      />
+    <View style={styles.listContainer}>
+      <Circle />
       <View style={{flexDirection: 'column', flex: 1}}>
         <Title title={title} />
         <View
@@ -105,15 +96,7 @@ const Divider = () => {
 
 const Title = ({title}: {title: string}) => {
   return (
-    <Text
-      style={{
-        color: Colors.white,
-        fontFamily: Fonts.LatoRegular,
-        fontSize: 16,
-        flex: 1,
-      }}
-      numberOfLines={2}
-      ellipsizeMode={'tail'}>
+    <Text style={styles.title} numberOfLines={2} ellipsizeMode={'tail'}>
       {title}
     </Text>
   );
@@ -121,70 +104,43 @@ const Title = ({title}: {title: string}) => {
 
 const DateTask = ({dateTime}: {dateTime: string}) => {
   return (
-    <Text
-      style={{
-        color: Colors.gray2,
-        fontFamily: Fonts.LatoRegular,
-        fontSize: 14,
-      }}>
+    <Text style={styles.dateText}>
       Today At {moment(dateTime).format('h:mm')}
     </Text>
   );
 };
 
-const FlagCategory = ({icon, label, color}) => {
+const FlagCategory = ({
+  icon,
+  label,
+  color,
+}: {
+  icon: ImageSourcePropType;
+  label: string;
+  color: string;
+}) => {
   return (
-    <View
-      style={{
-        backgroundColor: color,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 6,
-        marginRight: 12,
-        height: 29,
-        padding: 8,
-      }}>
-      <Image source={icon} style={{width: 14, height: 14, marginRight: 5}} />
-      <Text
-        style={{
-          fontFamily: Fonts.LatoRegular,
-          fontSize: 12,
-          color: Colors.white,
-        }}>
-        {label}
-      </Text>
+    <View style={[styles.flagContainer, {backgroundColor: color}]}>
+      <Image source={icon} style={styles.priorityIcon} />
+      <Text style={styles.priorityText}>{label}</Text>
     </View>
   );
 };
 
-const PriorityCategory = ({taskPriority}) => {
+const PriorityCategory = ({taskPriority}: {taskPriority: string}) => {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 8,
-        borderWidth: 1,
-        borderColor: Colors.purple2,
-        borderRadius: 4,
-        height: 29,
-      }}>
+    <View style={styles.priorityContainer}>
       <Image
         source={require('../../assets/icons/flag.png')}
-        style={{width: 14, height: 14, marginRight: 5}}
+        style={styles.priorityIcon}
       />
-      <Text
-        style={{
-          fontFamily: Fonts.LatoRegular,
-          fontSize: 12,
-          color: Colors.white,
-        }}>
-        {taskPriority}
-      </Text>
+      <Text style={styles.priorityText}>{taskPriority}</Text>
     </View>
   );
+};
+
+const Circle = () => {
+  return <View style={styles.circle} />;
 };
 
 const styles = StyleSheet.create({
@@ -204,5 +160,61 @@ const styles = StyleSheet.create({
     fontFamily: Fonts.LatoRegular,
     fontSize: 16,
     marginTop: 10,
+  },
+  circle: {
+    borderRadius: 8,
+    width: 16,
+    height: 16,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginRight: 12,
+  },
+  listContainer: {
+    backgroundColor: Colors.black2,
+    minHeight: 72,
+    borderRadius: 4,
+    alignItems: 'center',
+    padding: 8,
+    flexDirection: 'row',
+  },
+  title: {
+    color: Colors.white,
+    fontFamily: Fonts.LatoRegular,
+    fontSize: 16,
+    flex: 1,
+  },
+  dateText: {
+    color: Colors.gray2,
+    fontFamily: Fonts.LatoRegular,
+    fontSize: 14,
+  },
+  flagContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 6,
+    marginRight: 12,
+    height: 29,
+    padding: 8,
+  },
+  priorityContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 8,
+    borderWidth: 1,
+    borderColor: Colors.purple2,
+    borderRadius: 4,
+    height: 29,
+  },
+  priorityIcon: {
+    width: 14,
+    height: 14,
+    marginRight: 5,
+  },
+  priorityText: {
+    fontFamily: Fonts.LatoRegular,
+    fontSize: 12,
+    color: Colors.white,
   },
 });
