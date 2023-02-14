ac89de0cfdf0d8456fed83826a17b72d32393132
Hamam <zero.saturnus@gmail.com>
2023-02-14 12:06:29 +0700

    add new color

--- src/utils/Colors.tsx
+++ src/utils/Colors.tsx
@@ -1,6 +1,7 @@
 const Colors = {
   purple: '#8875FF',
   purple2: '#8687E7',
+  purple3: '#809CFF',
   white: '#ffffff',
   white1: 'rgba(255, 255, 255, 0.87)',
   white2: 'rgba(255, 255, 255, 0.44)',
@@ -10,6 +11,14 @@ const Colors = {
   black: '#121212',
   black1: '#1D1D1D',
   black2: '#363636',
+  green1: '#CCFF80',
+  green2: '#80FFA3',
+  orange1: '#FF9680',
+  cyan1: '#80FFFF',
+  pink1: '#FF80EB',
+  blue1: '#80D1FF',
+  yellow1: '#FFCC80',
+  red1: '#FF8080',
 };
 
 export default Colors;
