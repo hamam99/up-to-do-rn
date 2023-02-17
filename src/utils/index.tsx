8a9da57176916d5031b7c8978b6c1f0ca4d79bec
Hamam <zero.saturnus@gmail.com>
2023-02-17 23:50:18 +0700

    add realtime db

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,6 +2,7 @@ import Auth from './Auth';
 import Colors from './Colors';
 import DateTimeHelper from './DateTimeHelper';
 import Fonts from './Fonts';
+import RealtimeDatabase from './RealtimeDatabase';
 import Validation from './Validation';
 
-export {Colors, Fonts, Validation, Auth, DateTimeHelper};
+export {Colors, Fonts, Validation, Auth, DateTimeHelper, RealtimeDatabase};
